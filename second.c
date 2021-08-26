daf31867092ca766e31432ff7fcb1b9816930987
krallice <sroz9960@gmail.com>
2021-08-26 16:33:48 +1000

    adds second.c

---
+++ second.c
@@ -0,0 +1,18 @@
+void second(int a) {
+
+    a + 4;
+
+    int x = 10;
+    x++;
+}
+
+int main(void) {
+
+    // This is a test
+
+    second(a);
+    int a = 4;
+    int b = 7;
+
+    return (a+b);
+}
